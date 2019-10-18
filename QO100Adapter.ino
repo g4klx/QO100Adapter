4572a82b43c2f60c6e18c50aef96c2724802e05b
Jonathan Naylor <jonathan.naylor@igence.com>
2019-10-18 10:10:18 +0100

    Add extra processing for the radio.

--- QO100Adapter.ino
+++ QO100Adapter.ino
@@ -7,41 +7,54 @@
 // Setup the I2C LCD Display
 LiquidCrystal_I2C lcd(0x27, 20, 4);
 
-// SoftwareSerial Yaesu_Serial(2, 3); // RX, TX
-AltSoftSerial Yaesu_Serial;
+// SoftwareSerial radioSerial(2, 3); // RX, TX
+AltSoftSerial radioSerial;
 
 // Setup a RoraryEncoder for pins A2 and A3:
 RotaryEncoder encoder(A3, A2);
 
+const int CYCLE_TIME = 20;
+
+const int ADDRESS = 1;
+
+const int DISPLAY_TIME  = 2000 / CYCLE_TIME;
+const int SDR_POLL_TIME = 1000 / CYCLE_TIME;
+
 long XIT ; // Xmitter Incremental Tuning
 
-bool Encoder_was_touched = false;
+bool encoderTouched = false;
 
 bool sdrConnected = false;
 bool radioConnected = true;
 
-unsigned long rxFreqKHz = 0UL;
-unsigned long txFreqKHz = 0UL;
-unsigned long ifFreqKHz = 0UL;
+unsigned long rxFreqHz = 0UL;
+unsigned long txFreqHz = 0UL;
+unsigned long ifFreqHz = 0UL;
 
 char sdrBuffer[50];
 int sdrIndex = 0;
-int count = 0;
+int sdrCount = 0;
+
+byte radioBuffer[5];
+int radioIndex = 0;
+int radioCount = 0;
+
+int displayCount = 0;
 
 void setup()
 {
   Serial.begin(57600, SERIAL_8N1); // open the serial port that will be connected with SDR Console at 57600, 8 bits, no parity, 1 stop bit
 
-  Yaesu_Serial.begin(38400);// set the data rate for the SoftSerial port
+  radioSerial.begin(38400);// set the data rate for the SoftSerial port
 
   // For the rotary encoder that works on interrupts
   PCICR |= (1 << PCIE1);    // This enables Pin Change Interrupt 1 that covers the Analog input pins or Port C.
   PCMSK1 |= (1 << PCINT10) | (1 << PCINT11);  // This enables the interrupt for pin 2 and 3 of Port C.
   pinMode(A1, INPUT);    // For the Rotary encoder switch
 
-  //Reading last used XIT from EEPROM.
-  // XIT = EEPROM_Read(1); // Gets Xmitter Incremental Tuning stored in EEPROM
-  encoder.setPosition(XIT / 10);  // for initiate the encoder value
+  // Read last used XIT from EEPROM.
+  EEPROMRead();
+  encoder.setPosition(XIT / 10);
 
   // initialize the LCD Screen
   lcd.init();
@@ -51,25 +64,23 @@ void setup()
   lcd.setCursor(0, 1); lcd.print(" Es hail 2 / QO-100");
   lcd.setCursor(0, 2); lcd.print("Controller by G4KLX");
   lcd.setCursor(0, 3); lcd.print("      Ver 1.00");
+  displayCount = 0;
+
   delay(1000);
 
   showConnections();
-
-  // Check_Connection_With_SDR_Console();// as it says
-  // Check_Connection_With_FT818ND();
-  // Set_SDR_RX_Frequency_to_Upper_Beacon();// as it says
-  // Unlock_FT818ND(); //as it says
-  // Set_FT818ND_Operating_Mode_to_USB();//as it says
 }
 
 // The Interrupt Service Routine for Pin Change Interrupt 1
 // This routine will only be called on any signal change on A2 and A3
 ISR(PCINT1_vect)
 {
-  encoder.tick(); // just call tick() to check the state.
-  int newPos = encoder.getPosition();
-  XIT = newPos * 20;
-  Encoder_was_touched = true;
+  if (sdrConnected && radioConnected) {
+    encoder.tick(); // just call tick() to check the state.
+    int newPos = encoder.getPosition();
+    XIT = newPos * 20;
+    encoderTouched = true;
+  }
 }
 
 void loop()
@@ -83,31 +94,68 @@ void loop()
         processID();
       } else {
         processFreq();
-        calculateFreq();
-        showFrequency();
+        if (calculateFreq()) {
+          showFrequency();
+		  setRadioFrequency();
+        }
       }
 
       sdrIndex = 0;
     }
   }
 
-  count++;
-  if (count >= 50) {
+  if (radioSerial.available()) {
+    int c = radioSerial.read();
+	radioBuffer[radioIndex++] = c;
+
+    if (radioIndex == 5) {
+      if (!radioConnected)
+        processRadio();
+    }
+
+    radioIndex = 0;
+  }
+
+  sdrCount++;
+  if (sdrCount >= SDR_POLL_TIME) {
     if (!sdrConnected)
       Serial.write("ID;");
-    else
+    else if (radioConnected)
       Serial.write("FA;");
 
-    count = 0;
+    sdrCount = 0;
+  }
+  
+  radioCount++;
+  if (radioCount >= RADIO_POLL_TIME) {
+    if (!radioConnected)
+      readRXStatus();
+
+    radioCount = 0;
   }
 
-  if (Encoder_was_touched) {
-    calculateFreq();
-    showFrequency();
-    Encoder_was_touched = false;
+  if (encoderTouched) {
+    if (calculateFreq()) {
+      showFrequency();
+      setRadioFrequency();
+	}
+    encoderTouched = false;
   }
 
-  delay(20);
+  if (displayCount > 0) {
+    displayCount--;
+	if (displayCount == 0)
+      lcd.noBacklight();
+  }
+
+  if (sdrConnected && radioConnected) {
+    if (digitalRead(A1) == LOW) {
+      saveXIT();
+	  showFrequency();
+	}
+  }
+
+  delay(CYCLE_TIME);
 }
 
 void processID()
@@ -122,15 +170,22 @@ void processFreq()
 {
   // if (sdrIndex == 14 && memcmp(sdrBuffer, "FA10489", 7) == 0) {
     // The RX frequency in Hertz minus the 10 GHz part.
-    rxFreqKHz = (sdrBuffer[8]  - '0') * 100000UL    + (sdrBuffer[9]  - '0') * 10000UL    + (sdrBuffer[10] - '0') * 1000UL +
-                (sdrBuffer[11] - '0') * 100UL       + (sdrBuffer[12] - '0') * 10UL       + (sdrBuffer[13] - '0');
+    rxFreqHz = (sdrBuffer[8]  - '0') * 100000UL    + (sdrBuffer[9]  - '0') * 10000UL    + (sdrBuffer[10] - '0') * 1000UL +
+               (sdrBuffer[11] - '0') * 100UL       + (sdrBuffer[12] - '0') * 10UL       + (sdrBuffer[13] - '0');
 }
 
-void calculateFreq()
+bool calculateFreq()
 {
-    txFreqKHz = rxFreqKHz + 2400000000UL - 500000UL;  
+  txFreqHz = rxFreqHz + 2400000000UL - 500000UL;  
 
-    ifFreqKHz = rxFreqKHz + XIT + 432000000UL - 500000UL;
+  unsigned long new IFFreqHz = rxFreqHz + XIT + 432000000UL - 500000UL;
+
+  if (newIFFreqHz != ifFreqHz) {
+    ifFreqHz = newIFFreqHz;
+    return true;
+  } else {
+    return false;
+  }
 }
 
 void showConnections()
@@ -139,6 +194,8 @@ void showConnections()
     return;
 
   lcd.clear();
+  lcd.backlight();
+
   lcd.setCursor(0, 0);
   lcd.print("Waiting for:");
 
@@ -154,6 +211,8 @@ void showConnections()
     lcd.setCursor(0, n);
     lcd.print("  FT-817");
   }
+
+  displayCount = DISPLAY_TIME;
 }
 
 void showFrequency()
@@ -164,34 +223,22 @@ void showFrequency()
   char buf[20];
 
   lcd.clear();
+  lcd.backlight();
 
-  sprintf(buf, "%lu", rxFreqKHz);
+  sprintf(buf, "RX:  10489lu Hz", rxFreqHz);
 
   lcd.setCursor(0, 0);
-  lcd.print("RX: 10489.");
-  lcd.write(buf[0]); lcd.write(buf[1]); lcd.write(buf[2]);
-  lcd.write(buf[3]); lcd.write(buf[4]); lcd.write(buf[5]);
-  lcd.print(" MHz");
+  lcd.print(buf);
 
-  sprintf(buf, "%lu", txFreqKHz);
+  sprintf(buf, "TX:     %lu Hz", txFreqHz);
 
   lcd.setCursor(0, 1);
-  lcd.print("TX:  ");
-  lcd.write(buf[0]); lcd.write(buf[1]); lcd.write(buf[2]); lcd.write(buf[3]);
-  lcd.print(".");
-  lcd.write(buf[4]); lcd.write(buf[5]); lcd.write(buf[6]);
-  lcd.write(buf[7]); lcd.write(buf[8]); lcd.write(buf[9]);
-  lcd.print(" MHz");
+  lcd.print(buf);
 
-  sprintf(buf, "%lu", ifFreqKHz);
+  sprintf(buf, "IF:     %lu Hz", ifFreqHz);
 
   lcd.setCursor(0, 2);
-  lcd.print("IF:   ");
-  lcd.write(buf[0]); lcd.write(buf[1]); lcd.write(buf[2]);
-  lcd.print(".");
-  lcd.write(buf[3]); lcd.write(buf[4]); lcd.write(buf[5]);
-  lcd.write(buf[6]); lcd.write(buf[7]); lcd.write(buf[8]);
-  lcd.print(" MHz");
+  lcd.print(buf);
 
   if (XIT < 0L)
     sprintf(buf, "-%ld", -XIT);
@@ -204,4 +251,89 @@ void showFrequency()
     lcd.write(' ');
   lcd.print(buf);
   lcd.print("  Hz");
+
+  displayCount = DISPLAY_TIME;
+}
+
+void EEPROMWrite()
+{
+  byte four  = ((XIT >> 0)  & 0xFF);
+  byte three = ((XIT >> 8)  & 0xFF);
+  byte two   = ((XIT >> 16) & 0xFF);
+  byte one   = ((XIT >> 24) & 0xFF);
+
+  EEPROM.write(ADDRESS + 0, four);
+  EEPROM.write(ADDRESS + 1, three);
+  EEPROM.write(ADDRESS + 2, two);
+  EEPROM.write(ADDRESS + 3, one);
+}
+
+void EEPROMRead()
+{
+  long four  = EEPROM.read(ADDRESS + 0);
+  long three = EEPROM.read(ADDRESS + 1);
+  long two   = EEPROM.read(ADDRESS + 2);
+  long one   = EEPROM.read(ADDRESS + 3);
+
+  XIT = ((four << 0) & 0xFF) + ((three << 8) & 0xFFFF) + ((two << 16) & 0xFFFFFF) + ((one << 24) & 0xFFFFFFFF);
+}
+
+void saveXIT()
+{
+  EEPROMWrite();
+
+  for (int i = 0; i <= 3; i++) {
+    lcd.clear();
+    lcd.setCursor(0, 0); lcd.print ("####################");
+    lcd.setCursor(0, 1); lcd.print ("#   XIT saved to   #");
+    lcd.setCursor(0, 2); lcd.print ("#      EEPROM      #");
+    lcd.setCursor(0, 3); lcd.print ("####################");
+    delay(100);
+  }
+}
+
+void unlockRadio()
+{
+  radioSerial.write(0x00); // DATA1 = null
+  radioSerial.write(0x00); // DATA2 = null
+  radioSerial.write(0x00); // DATA3 = null
+  radioSerial.write(0x00); // DATA4 = null
+  radioSerial.write(0x80); // DATA5 = 80 (LOCK OFF Opcode)
+}
+
+void readRXStatus()
+{
+  radioSerial.write(0x00); // DATA1 = null
+  radioSerial.write(0x00); // DATA2 = null
+  radioSerial.write(0x00); // DATA3 = null
+  radioSerial.write(0x00); // DATA4 = null
+  radioSerial.write(0xE7); // DATA5 = E7 (Read RX Status Opcode)
+}
+
+void setRadioFrequency()
+{
+  byte one   = (((ifFreqHZ / 100000000UL) & 0x0FUL) << 4) | (((ifFreqHZ / 10000000UL) & 0x0FUL) << 0);
+  byte two   = (((ifFreqHZ / 1000000UL)   & 0x0FUL) << 4) | (((ifFreqHZ / 100000UL)   & 0x0FUL) << 0);
+  byte three = (((ifFreqHZ / 10000UL)     & 0x0FUL) << 4) | (((ifFreqHZ / 1000UL)     & 0x0FUL) << 0);
+  byte four  = (((ifFreqHZ / 100UL)       & 0x0FUL) << 4) | (((ifFreqHZ / 10UL)       & 0x0FUL) << 0);
+
+  radioSerial.write(one);
+  radioSerial.write(two);
+  radioSerial.write(three);
+  radioSerial.write(four);
+  radioSerial.write(0x01); // DATA5 = 01 (Set Frequency Opcode)
+}
+
+bool processRadio()
+{
+  unsigned int checksum = 0U;
+  for (int i = 0; i < 5; i++)
+    checksum += radioBuffer[i];
+
+  if (checksum > 0U) {
+    radioConnected = true;
+    return true
+  } else {
+    return false;
+  }
 }
